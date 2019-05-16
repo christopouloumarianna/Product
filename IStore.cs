2b3790faf0c29a79146bfa0465803ce35312ab3d
christopouloumarianna <50484642+christopouloumarianna@users.noreply.github.com>
2019-05-17 00:12:45 +0300

    Initial commit

---
+++ IStore.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Store1
+{
+    public interface IStore
+    {
+        void Buy(Product product);
+        void Sell(Product product);
+        double GetRevenue();
+
+    }
+    public class SimpleRetailStore : IStore
+    {
+        public double TotalBuyBalance { get; set; }
+        public double TotalSellBalance { get; set; }
+        public void Buy(Product product)
+        {
+            TotalBuyBalance += product.PriceWhenBuy;
+        }
+        public void Sell(Product product)
+        {
+            TotalSellBalance += product.PriceWhenSell;
+        }
+        public double GetRevenue()
+        {
+            return TotalSellBalance - TotalBuyBalance;
+        }
+
+        public SimpleRetailStore(double totalByeBalance, double totalSellBalance)
+        {
+            TotalBuyBalance = totalByeBalance;
+            TotalSellBalance = totalSellBalance;
+        }
+
+        public SimpleRetailStore()
+        {
+        }
+    }
+    public class InventoryRetailStore : IStore
+    {
+        private List<Product> Inventory { get; set; }
+        double TotalRevenue;
+        private Product product;
+
+        public InventoryRetailStore()
+        {
+            Inventory = new List<Product>();
+        }
+        public void Buy(Product product)
+        {
+            Inventory.Add(product);
+            TotalRevenue -= product.PriceWhenBuy;
+        }
+        public void Sell(Product product)
+        {
+            Inventory.Remove(product);
+            TotalRevenue -= product.PriceWhenSell;
+        }
+        public void GetInventory()
+        {
+            foreach (Product p in Inventory)
+            {
+                Console.WriteLine(p);
+            }
+        }
+        public double GetRevenue()
+        {
+
+
+            return TotalRevenue;
+
+        }
+
+
+        public void Reset()
+        {
+            Inventory = new List<Product>();
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Store1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product x1 = new Product(1, "Painting Picasso. Guernica", 100, 1000);
+            Product x2 = new Product(2, "Painting Tsarouxis. Naftis A", 200, 2000);
+            Product x3 = new Product(3, "Chair. Luis XV", 100, 1000);
+            Product x4 = new Product(4, "Vase. Cubic", 500, 5000);
+            var simple = new SimpleRetailStore();
+            simple.Buy(x1); simple.Buy(x3); simple.Sell(x1);
+            Console.WriteLine(simple.GetRevenue());
+            var invStore = new InventoryRetailStore();
+            invStore.Buy(x1); invStore.Buy(x3); invStore.Sell(x1);
+            Console.WriteLine(invStore.GetRevenue());
+            invStore.Sell(x2);
+            invStore.GetInventory();
+        }
+    }
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double PriceWhenBuy { get; set; }
+        public double PriceWhenSell { get; set; }
+
+        public Product(int id, string name, double priceWhenBuy, double priceWhenSell)
+        {
+            Id = id;
+            Name = name;
+            PriceWhenBuy = priceWhenBuy;
+            PriceWhenSell = priceWhenSell;
+        }
+
+        public override string ToString()
+        {
+            return $" {Id} {Name} {PriceWhenBuy} {PriceWhenSell}";
+        }
+
+        public bool Equals(Product other)
+        {
+            return Id == other.Id && Name == other.Name;
+        }
+    }
+}
+
+
